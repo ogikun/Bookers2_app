f2f9da67fed25d8c8e8ad473d7268f86ae29a5a0
ogikun <g1621362@gmail.com>
2021-01-08 02:48:00 +0000

    [Add] test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,22 @@ class BooksController < ApplicationController
   def top
   end
   
+  def index
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'books#top'
+  root to: 'homes#home'
+  get 'home/about', to: 'homes#about'
+  resources :users, only: [:index, :show, :edit]
+  resources :books
+  
 end
