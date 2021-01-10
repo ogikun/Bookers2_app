63e7e1ccfc29542f1da62f3fc8cbf53bd5f00462
ogikun <g1621362@gmail.com>
2021-01-10 13:38:06 +0000

    [Update] layouts

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'be7ead315ee59439d3976523ff74d1b8f57c9271d6b586789ff8ac5fce01d269d37b0f36bdd5c8a7acc0b32f0671f5873f432c2447dde8b6cfedc0a063a463ca'
